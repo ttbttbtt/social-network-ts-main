f7dac01a789ed8cac03bba380748effc17a99969
ttbttbtt <docent2212@mail.ru>
2023-11-18 01:19:09 +0300

    login

--- src/pages/LoginPage/LoginPage.style.ts
+++ src/pages/LoginPage/LoginPage.style.ts
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const StyledLoginPage = styled.div`
-  box-shadow: 0 0 10px var(--light-gray);
+  box-shadow: 0 0 10px ${(props) => props.theme.colors.lightGray};
   border-radius: 20px;
-  background-color: var(--elems-bgc);
+  background-color: ${(props) => props.theme.colors.elemsBgc};
   margin: 0 auto;
   width: 80%;
   max-width: 500px;
@@ -14,15 +14,40 @@ export const StyledLoginPage = styled.div`
     margin-bottom: 50px;
   }
 
-  a {
-    display: inline-block;
+  input {
+    display: block;
     width: 100%;
-    text-align: right;
-    text-decoration: none;
-    color: var(--gray);
+    margin-bottom: 20px;
+    border: 2px solid ${(props) => props.theme.colors.disabledBgc};
+    background-color: transparent;
 
-    &:hover {
-      text-decoration: underline;
+    &:last-child {
+      margin-bottom: 40px;
+    }
+
+    &:is(:hover, :focus) {
+      border-color: ${(props) => props.theme.colors.primeColor};
     }
   }
+
+  button {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+
+   a {
+     display: inline-block;
+     width: 100%;
+     text-align: right;
+     text-decoration: none;
+     color: ${(props) => props.theme.colors.gray};
+
+     &:hover {
+       text-decoration: underline;
+     }
+   }
+
+ @media (max-width: 530px) {
+     width: 100%;
+ }
 `;
